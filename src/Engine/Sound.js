703d6e7127d69a6c067716052467fef6e85b84b3
Nick Marino <contact@nmarino.dev>
2023-10-15 01:24:07 -0400

    update ost

--- src/Engine/Sound.js
+++ src/Engine/Sound.js
@@ -4,9 +4,9 @@
 
 // OST:
 import intro from '../../assets/audio/intro.mp3'
-import courtyard from "../../assets/audio/ost/swamp.mp3"
-import cliffs from "../../assets/audio/swamp.mp3"
-import swamp from '../../assets/audio/swamp.mp3'
+import courtyard from "../../assets/audio/ost/courtyard.mp3"
+import cliffs from "../../assets/audio/ost/cliffs.mp3"
+import swamp from '../../assets/audio/ost/swamp.mp3'
 
 // firstHallway
 // wanderingAroundOutside
